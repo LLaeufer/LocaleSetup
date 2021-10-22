50866c41c3c364ccee5aed501bfe798dbd75361e
Leon Läufer <88783053+LLaeufer@users.noreply.github.com>
2021-10-22 12:19:11 +0200

    Create setupLocale.sh

---
+++ setupLocale.sh
@@ -0,0 +1,10 @@
+localectl set-locale LANG=en_US.UTF-8
+localectl set-locale LC_ADDRESS=de_DE.UTF-8
+localectl set-locale LC_IDENTIFICATION=de_DE.UTF-8
+localectl set-locale LC_MEASUREMENT=de_DE.UTF-8
+localectl set-locale LC_MONETARY=de_DE.UTF-8
+localectl set-locale LC_NAME=de_DE.UTF-8
+localectl set-locale LC_NUMERIC=de_DE.UTF-8
+localectl set-locale LC_PAPER=de_DE.UTF-8
+localectl set-locale LC_TELEPHONE=de_DE.UTF-8
+localectl set-locale LC_TIME=de_DE.UTF-8
